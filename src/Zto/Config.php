4de03bed220a2969bd5b6f9e33555e79bf43dc3c
luffyzhao <luffyzhao@vip.126.com>
2021-08-23 18:09:08 +0800

    1

--- src/Zto/Config.php
+++ src/Zto/Config.php
@@ -6,8 +6,8 @@ use LExpress\ConfigInterFace;
 class Config implements ConfigInterFace
 {
     public $apiUrl = 'https://gjapi.zt-express.com/api/import/init';
-    public $companyId = 'CSBLGYL1608327E3';
-    public $key = 'AA0AB11F73ED98EE2C';
-    public $platformSource = 1336;
-    public $warehouseCode = 'CSBL';
+    public $companyId = '';
+    public $key = '';
+    public $platformSource;
+    public $warehouseCode;
 }
