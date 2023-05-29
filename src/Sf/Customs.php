ae231513ed74808155b58344ff217fae8eec86e7
luffyzhao <luffyzhao@vip.126.com>
2023-05-29 18:46:27 +0800

    邮政加入进来

---
+++ src/Sf/Customs.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LExpress\Sf;
+
+use LExpress\ConfigInterFace;
+use LExpress\Info;
+use LExpress\OperateInterFace;
+use LExpress\Response;
+
+class Customs implements OperateInterFace
+{
+
+    /**
+     * @var ConfigInterFace
+     */
+    private $config;
+    /**
+     * @var Info
+     */
+    private $data;
+
+    public function __construct(ConfigInterFace $config, Info $data)
+    {
+        $this->config = $config;
+        $this->data = $data;
+    }
+
+    public function handle(): Response
+    {
+        return new Response(true);
+    }
+
+    public function getBody()
+    {
+        return [];
+    }
+}
